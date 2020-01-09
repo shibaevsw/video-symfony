dd5c1fd6673a2e2fbb8a41c3e4d45c52ba28da77
shibaevsw <shibaevsw@gmail.com>
2020-01-09 11:53:45 +0300

    started to develop advanced symfony app. Created FrontConroller and routes for index and video-list. Moved temlates for them

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends AbstractController
+{
+    /**
+     * @Route("/", name="main_page")
+     */
+    public function index()
+    {
+        return $this->render('front/index.html.twig');
+    }
+
+    /**
+     * @Route("/video-list", name="video_list")
+     */
+    public function videoList()
+    {
+        return $this->render('front/video_list.html.twig');
+    }
+
+    /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function videoDetails()
+    {
+        return $this->render('front/video_details.html.twig');
+    }
+}
